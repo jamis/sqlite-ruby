d75e515f4ca8b22bce5a434b5aedd29935c25f31
Jamis Buck <jamis@37signals.com>
2005-01-25 21:54:37 +0000

    Added out-of-the-box support for /usr/local installations.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,6 @@
 require 'mkmf'
 
-dir_config( "sqlite" )
+dir_config( "sqlite", "/usr/local", "/usr/local" )
 have_library( "sqlite" )
 
 if have_header( "sqlite.h" ) and have_library( "sqlite", "sqlite_open" )
